7f10c95bd817cb305d00433a280dff9082156e03
Andrew_ST <andrew116st@gmail.com>
2023-01-27 11:53:38 +0400

    DAY_1

--- src/DAY_1/easterBunny2.java
+++ src/DAY_1/easterBunny2.java
@@ -3,9 +3,11 @@ package DAY_1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
-public class easterBunny {
+
+public class easterBunny2 {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		File file = new File("bunny_road");
@@ -25,13 +27,13 @@ public class easterBunny {
 		int y = 0; 			// left
 		int route = 0;		// route
 		
-		
+		HashSet<String> coords = new HashSet<String>();
 		
 		for (int i = 0; i < instructions.length; i++ ) {
 		
 			String instruction = instructions[i];
 			
-			int step = Integer.parseInt(instruction.substring(1));		
+			int step = Integer.parseInt(instruction.substring(1));		// шаг
 			char turn = instruction.charAt(0);
 	
 			switch (turn) {
@@ -58,39 +60,49 @@ public class easterBunny {
 				
 			}
 				
+			for (int k =0; k < step; k++) {
+				switch (route) {
+		
+					case 0:
+						y++;
+						break;
 			
-			switch (route) {
+					case 90:
+						x++;
+						break;
+			
+					case 180:
+						y--;
+						break;
+			
+					case 270:
+						x--;
+						break;
+			
+					default:
+						System.out.println("error route");
+			
+						return;
+						
+				}
+				
+				String coord = x + "  " + y;
+				if (coords.contains(coord)) {
 	
-				case 0:
-					y = y + step;
-					break;
-		
-				case 90:
-					x = x + step;
-					break;
-		
-				case 180:
-					y = y - step;
-					break;
-		
-				case 270:
-					x = x - step;
-					break;
-		
-				default:
-					System.out.println("error route");
-		
+					int fullpath = Math.abs(x) + Math.abs(y);
+	
+					System.out.println(coord);
+					System.out.println();
+					System.out.println("Fullpath - " + fullpath);
+					
 					return;
-		
+				}
+				
+				coords.add(coord);
 			}
-
 		}
 		
-		int fullpath = Math.abs(x) + Math.abs(y);
-				
-		System.out.println(x + "  "  +  y);
-		System.out.println();
-		System.out.println("Fullpath - " + fullpath);
+		
 	}
 
 }
